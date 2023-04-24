c33c599349658d1186e9090a3ed5f9c165102553
Ashley Kosik <ashley.kosik@gmail.com>
2023-04-24 14:52:45 -0400

    Day 16 created first server

---
+++ back-end-1-demo/server/server.js
@@ -0,0 +1,36 @@
+//imports & packages
+const express = require('express') 
+const cors = require('cors')
+
+//initialize express
+const app = express()
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+//database
+const inventory = ['greeting card', 'table', 'chair', 'milk', 'boat', 'cookies', 'cheese', 'shampoo', 'conditioner', 'face wash']
+
+//endpoints 
+// all inventory
+app.get('/api/inventory', (req, res) => {res.status(200).send(inventory)})
+// enpoint with parameter
+app.get('/api/inventory/:id', (req, res) => {
+    //console.log(req.params.id)
+    const {id} = req.params
+    res.status(200).send(inventory[id])
+})
+
+app.get('/api/inventory/:id', (req, res) => {
+    console.log(req.query);
+    if (req.query.item) {
+        const filteredItems = inventory.filter((invItem) => invItem.toLowerCase().includes(req.query.item.toLowerCase()))
+        res.status(200).send(filteredItems) 
+    } else {
+        res.status(200).send(inventory) 
+    }
+})
+
+//runs the server
+app.listen(5050, () => console.log(`jammin on 5050`))
